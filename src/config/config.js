c3ad4c5950021e36bf924db8472779fe97ebd705
Carlos Triana <carlosandrestriana8@gmail.com>
2026-06-04 22:12:57 -0500

    se termino de crear la conexion a la base de datos, se agrego la estructura de carpetas para poder empezar a trabajar en los diferentes modulos

---
+++ src/config/config.js
@@ -0,0 +1,23 @@
+const { connection } = require("mongoose");
+const  {Client}   = require("pg");
+
+
+const dbConnection = async () => {
+    try{
+        const client = new Client({
+            connectionString: process.env.DB_URL,
+        });
+        await client.connect();
+        console.log("Base de datos inicializada exitosamente");
+        return client
+
+    }catch (error){
+        console.log(error);
+        throw new Error(' Error al inicializar la base de datos');   
+    }
+}
+
+
+module.exports = {
+    dbConnection
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const cors = require("cors");
+const {dbConnection} = require('./config/config');
+
+const PORT = process.env.PORT
+
+app.use( express.json());
+app.use( cors());
+
+// app.use('/api', require('./routes/auth.routes'));
+
+
+dbConnection();
+
+app.listen(PORT, function(){
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+});
