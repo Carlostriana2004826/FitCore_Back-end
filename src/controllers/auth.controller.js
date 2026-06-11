19fbedc2ca9c9dcd5f589d88ed4510dc6b66fbc8
Carlos Triana <carlosandrestriana8@gmail.com>
2026-06-10 22:36:06 -0500

    se sigue creando la configuracion para la creacion e ingresos de usuario, para mas seguridad se termino de configurar un ORM (Prisma) para a futuro el codigo pueda ser mejor y mantenible

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,6 +10,8 @@ const login =  async (req, res)  => {
                 msg:'Error porfavor registrarse'
             });
         }
+
+        // const isValiedPassword = 
     }catch(error){
         console.log(error)
         return res.status(500).json({
@@ -20,8 +22,47 @@ const login =  async (req, res)  => {
 
 } 
 
-const register = (req, res) => {
+const register = async  (req, res) => {
     const inputData = req.body
+    try{
+        const userFound = await userServices.findUserById(inputData.cedula)
+    if(userFound){
+        return res.status(409).json({
+            ok:false,
+            msg:'Erro el usuario ya esta registrado'
+        })
+    }
+
+    const foundRole = await userServices.findRolUser("USER");
+
+    if (!foundRole) {
+        return res.status(500).json({
+            ok: false,
+            msg: "El rol por defecto no existe"
+        });
+    }
+
+    const dataRegister = {
+        cedula: inputData.cedula,
+        nombre: inputData.nombre,
+        email: inputData.email,
+        password_hash: inputData.password_hash,
+        telefono: inputData.telefono,
+        rol_id: foundRole.id
+    };
+
+    const createUser = await userServices.userCreated(dataRegister);
+    if (createUser){
+        return res.status(201).json({
+        ok: true,
+        user: userCreated
+    });
+    }
+    
+    
+    }catch (error){
+        console.log(error)
+    }
 }
 
 module.exports = {
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,30 @@
-const {client} = require("../config/config/")
+const { PrismaClient } = require("../generated/prisma");
+const prisma = new PrismaClient();
 
-const findUserById = async(cedula) => {
-    const result  = await client.query(
-        "SELECT * FROM usuarios WHERE cedula = $1",
-        [cedula]
-    );
-    return result.rows[0];
+const findUserById = async (cedula) => {
+    return await prisma.usuarios.findUnique({
+        where: {
+            cedula
+        }
+    });
+};
+
+const findRolUser = async (rol) => {
+    return await prisma.rol.findUnique({
+        where: {
+            nombre_rol: rol
+        }
+    });
+};
+
+const userCreated = async (dataRegister) => {
+    return await prisma.usuarios.create({
+        data: dataRegister
+    });
 };
 
 module.exports = {
-    findUserById
+    findUserById,
+    findRolUser,
+    userCreated
 }
